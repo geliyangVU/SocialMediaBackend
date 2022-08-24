166e015620216dfce065059ec0140c8bde05e9b4
Geli Yang <gyang5@villanova.edu>
2022-08-23 21:55:03 -0400

    Implement login function, use bcrypt.compare to authenticate user

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,6 @@ const bcrypt = require("bcrypt");
 //REGISTER
 router.post("/register", async (req, res) => {
   try {
-
     //use bcrypt to generate a hashed password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -20,6 +19,25 @@ router.post("/register", async (req, res) => {
     //save user and respond
     const user = await newUser.save();
     res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    //use bcrypt.compare to compare userpassword and hashed password.
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("wrong password");
+
+    res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
   }
