c1d82be0c0e5b81a76faef62c79d1b5521a99826
Geli Yang <gyang5@villanova.edu>
2022-08-23 23:32:57 -0400

    implemented update method for user

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,32 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
-router.get("/", (req,res) => {
-    res.send("user route");
+//update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    //if user password is included, rehash and update password.
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+
+    try {
+      //
+      const userToBeUpdated = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can update only your account!");
+  }
 });
-
 
 module.exports = router;
