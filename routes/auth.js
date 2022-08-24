b9db38250f6b652035d8be24cf701af81d4ae2d1
Geli Yang <gyang5@villanova.edu>
2022-08-23 21:24:27 -0400

    test register user locally, created a user in mongodb database

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,23 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("auth route");
+//REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    //create new user
+    //test
+    const newUser = new User({
+      username: "GeliYang",
+      email: "geliyang1995@gmail.com",
+      password: "passwordfornow",
+    });
+
+    //save user and respond
+    const user = await newUser.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
