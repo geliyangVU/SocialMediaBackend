ce4f32187e16e13b9a6e62a442fd1e063a267b2a
chuanlingShi <94197901+chuanlingShi@users.noreply.github.com>
2022-08-25 01:31:41 -0400

    implemented dislike/like a post

--- routes/posts.js
+++ routes/posts.js
@@ -29,7 +29,6 @@ router.put("/:id", async (req, res) => {
 });
 
 //delete a post
-
 router.delete("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
@@ -45,3 +44,19 @@ router.delete("/:id", async (req, res) => {
 });
 
 module.exports = router;
+
+//like / dislike a post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
