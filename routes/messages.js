068a8b4ab18c451ad26010cea08b70ec2ae6123e
Geli Yang <gyang5@villanova.edu>
2022-08-27 13:41:43 -0400

    implemented create message endpoint

--- routes/messages.js
+++ routes/messages.js
@@ -1,4 +1,20 @@
 const router = require("express").Router();
 const Message = require("../models/Message");
 
+// create a message endpoint, needs to pass in conversationId, senderId and text message to this endpoint
+router.post("/", async (req, res) => {
+  const { conversationId, senderId, text } = req.body;
+  try {
+    const messageToBeCreated = new Message({
+      conversationId: conversationId,
+      senderId: senderId,
+      text: text,
+    });
+    const savedMessage = await messageToBeCreated.save();
+    res.status(201).json(savedMessage);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
